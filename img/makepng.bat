e499459047997a9709d59e0cc98e48cbf0bf0e72
ttmdenso <105836018+ttmdenso@users.noreply.github.com>
2022-06-20 11:56:43 +0900

    img change

--- img/makepng.bat
+++ img/makepng.bat
@@ -1,7 +1,7 @@
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:128" -q 2 128.png
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:144" -q 2 144.png
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:152" -q 2 152.png
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:192" -q 2 192.png
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:256" -q 2 256.png
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:144" -q 2 512.png
-ffmpeg -i .\IMG_1159.jpg -vf "scale=-1:16" -q 2 16.png
+ffmpeg -i .\igusa.png -vf "scale=-1:128" -q 2 128.png
+ffmpeg -i .\igusa.png -vf "scale=-1:144" -q 2 144.png
+ffmpeg -i .\igusa.png -vf "scale=-1:152" -q 2 152.png
+ffmpeg -i .\igusa.png -vf "scale=-1:192" -q 2 192.png
+ffmpeg -i .\igusa.png -vf "scale=-1:256" -q 2 256.png
+ffmpeg -i .\igusa.png -vf "scale=-1:512" -q 2 512.png
+ffmpeg -i .\igusa.png -vf "scale=-1:16" -q 2 16.png
